2a0e32034614ba671417aec23553197f41ab51d1
Nathan Clark <nathanclark80@gmail.com>
2024-08-13 13:06:49 -0500

    `chore: Remove unused test case`

--- test/view_component_toolkit_test.rb
+++ test/view_component_toolkit_test.rb
@@ -6,8 +6,4 @@ class ViewComponentToolkitTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::ViewComponentToolkit::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
